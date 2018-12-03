a8f8217d9b7f376f7bf8c5dac36efbfef15fdfb0
Antonio Gagliarducci <gagliarducci.antonio@posteo.net>
2018-12-03 16:44:38 +0100

    Second program in java where I was tryng the System.out.Println

---
+++ Esercizi_1.5/Numeri.java
@@ -0,0 +1,10 @@
+public class Numeri
+{
+	public static void main(String[] args)
+	{
+		//I print simply three number in the console
+		System.out.println("3");
+		System.out.println("2");
+		System.out.println("1");
+	}
+}
