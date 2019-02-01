26725059c21881cf16fd3d6111af96c1eb1f6766
Antonio Gagliarducci <gagliarducci.antonio@posteo.net>
2019-02-01 13:24:31 +0100

    Add exercise 4 (EscapeSequencesComments2)

---
+++ javahard/Exercise 4/EscapeSequencesComments2.java
@@ -0,0 +1,9 @@
+public class EscapeSequencesComments2{
+	public static void main( String [] args )
+	{
+		// This exercise demonstrates escape sequences & comments (ALL IN ONE SOUT VERSION!)
+		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n \tLearn java the \"Hard\" Way\n Hello\n \n Jello\by\n \n Hard to belive, eh \n Surprised? /* abcde */ Or what did you expect? \n\\// -=- \\// \n \\\\ \\\\\\ \\\\\\\\ \n I hope you understand \"escape sequences\" now.\n" );
+		
+	}
+	
+}
