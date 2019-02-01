21aa245cac31db44ca81146719ee5a18e7e72166
Antonio Gagliarducci <gagliarducci.antonio@posteo.net>
2019-02-01 12:11:19 +0100

    Add first program in java that declare my intention to learn again the java language

---
+++ javahard/Exercise 1/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+	public static void main(String[] args)
+	{
+		System.out.println( "I, Antonio Gagliarducci, am determinied to learn how to code." );
+		System.out.println( "Today date is Thursday, January 24, 2018." );
+	}
+}
